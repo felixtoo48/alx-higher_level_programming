7183ef137ac3230d75ba2e87c9d4136242a209f2
felixtoo48 <felixtoo48@gmail.com>
2022-09-25 21:37:16 +0300

    including import file add_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-def add(a, b):
-    sum = a + b
-    return sum
+if __name__ = "__main__":
 
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
