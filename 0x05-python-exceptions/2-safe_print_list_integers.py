e48c6e30c8fae7415522b47079630a0b3e0a4e66
felixtoo48 <felixtoo48@gmail.com>
2022-10-13 22:29:28 +0300

    whitespace correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i]) + str(counter), end="")
+            print("{:d}".format(my_list[i]), end="")
             counter += 1
-        except IndexError:
-            break
+        except (ValueError, TypeError):
+            continue
     print("")
     return counter
-
