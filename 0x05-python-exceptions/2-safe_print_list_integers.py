ba5d7c4f1ba6567377cc2ba647539d4b1c3940fc
felixtoo48 <felixtoo48@gmail.com>
2022-10-13 22:22:10 +0300

    safe dividion

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]) + str(counter), end="")
+            counter += 1
+        except IndexError:
+            break
+    print("")
+    return counter
 
