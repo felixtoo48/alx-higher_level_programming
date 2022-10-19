5308e169e943e873a0c0d9f5c72ca170c41942eb
felixtoo48 <felixtoo48@gmail.com>
2022-10-20 01:38:26 +0300

    repr

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+Define class square
+"""
+
+
+class Rectangle:
+    """Define a rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """ Initialize a rectangle.
+
+        Args:
+        width (int): width of the new rectangle
+        height (int): the height of the new rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """ return printed representation of Rectangle
+
+        Rectangle is printed with # character.
+        """
+        if self.width == 0 or self.height == 0:
+            return ("")
+
+        shape = []
+        for i in range(self.height):
+            [shape.append("#") for j in range(self.width)]
+            if i != self.height - 1:
+                shape.append("\n")
+        return (''.join(shape))
+
+    def __repr__(self):
+        return repr("Rectangle({:d}, {:d})".format(self.width, self.height))
