4d86b8bffbf3fab4b1b352fd0ef15c6689428b05
felixtoo48 <felixtoo48@gmail.com>
2022-10-26 11:31:32 +0300

    number of instances in delete

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,8 @@ Define class square
 class Rectangle:
     """Define a rectangle class"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Initialize a rectangle.
 
@@ -15,6 +17,7 @@ class Rectangle:
         width (int): width of the new rectangle
         height (int): the height of the new rectangle
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
