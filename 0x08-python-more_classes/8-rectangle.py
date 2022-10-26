f67bff01f439cc1d4bf56677cceb3fd2d4ccf974
felixtoo48 <felixtoo48@gmail.com>
2022-10-26 14:40:00 +0300

    braces addition

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area == rect_2.area:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return max([str(rect_1), str(rect_2)])
