f375ad0a37222a7f122905683b9284eb8b2ad874
felixtoo48 <felixtoo48@gmail.com>
2022-10-27 00:53:13 +0300

    Rectaangle inheriting BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-Module 6-base_geometry
-to raise area of a base GeoMetry
+Module 8-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validator
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent
 """
 
 
