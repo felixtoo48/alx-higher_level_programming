cf0bbbf258dabff9f0826e39f0d8bebf1c4b2721
felixtoo48 <felixtoo48@gmail.com>
2022-10-27 00:19:37 +0300

    BaseGeometry area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+to raise area of a base GeoMetry
+"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """ not implemented"""
+        raise Exception("area() is not implemented")
