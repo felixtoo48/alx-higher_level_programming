0b37cf8ae16943c8d7ae3c34eb7e0d97b23b4647
felixtoo48 <felixtoo48@gmail.com>
2022-11-01 01:09:40 +0300

    indentation correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,10 @@ class Rectangle(Base):
             for j in range(self.width):
                 print('#', end='')
             print()
+
+    def __str__(self):
+        """ initialize str """
+        return "[{:s}] ({}) {:d}/{:d} - \
+{:d}/{:d}".format(self.__class__.__name__,
+                  self.id, self.__x, self.__y,
+                  self.__width, self.__height)
