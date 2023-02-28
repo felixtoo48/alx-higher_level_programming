7417e7668d9b2f6a8e8fa91261ae2c9dbddf04e8
felixtoo48 <felixtoo48@gmail.com>
2023-02-28 15:02:20 +0300

    update

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,7 +1,5 @@
-$(function () {
-  $.get('https://swapi.co/api/people/5/?format=json', function (resp, status) {
-    if (status === 'success') {
-      $('#character').text(resp.name);
-    }
-  });
+// JavaScript script that fetches the character name
+
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+    $('DIV#character').html(data['name']);
 });
