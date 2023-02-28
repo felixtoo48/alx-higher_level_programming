47cb5d3d53109c70653ce38e9fe29fe650541068
felixtoo48 <felixtoo48@gmail.com>
2023-02-28 15:03:30 +0300

    update

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,10 +1,12 @@
-$(function () {
-  $.get('https://swapi.co/api/films/?format=json', function (resp, status) {
-    if (status === 'success') {
-      let films = resp.results;
-      for (let idx in films) {
-        $('#list_movies').append('<li>' + films[idx].title + '</li>');
-      }
+// JavaScript script that fetches and lists the title for all movies
+
+$(() => {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', (data, textStatus) => {
+    if (textStatus === 'success') {
+      const films = data.results;
+      films.forEach(film => {
+        $('#list_movies').append('<li>' + film.title + '</li>');
+      });
     }
   });
 });
