bda9ea48d2b55ebb4d5f477785a9ff44c63c9420
felixtoo48 <felixtoo48@gmail.com>
2023-02-28 14:45:17 +0300

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file
 
-let url = process.argv[2];
-let filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filename, body, 'utf8');
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
