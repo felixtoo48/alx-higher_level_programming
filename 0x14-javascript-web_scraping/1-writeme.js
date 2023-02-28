06840c6af66b5da92983b2d272e57c37e46e3413
felixtoo48 <felixtoo48@gmail.com>
2023-02-28 14:53:29 +0300

    writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // A script that writes a string to a file
 
-let filename = process.argv[2];
-let text = process.argv[3];
 const fs = require('fs');
-fs.writeFile(filename, text, 'utf8', function (err) {
-  if (err) {
-    console.log(err);
+const file = process.argv[2];
+const write_ = process.argv[3];
+
+fs.writeFile(file, write_, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
   }
 });
