4c6e9b8c732e00a01ab6e348372c97e9b8ae86b7
felixtoo48 <felixtoo48@gmail.com>
2023-02-28 15:05:06 +0300

    update

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,9 @@
-$(function () {
-  $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Francisco%2C%20CA%22)&format=json', function (resp, status) {
-    if (status === 'success') {
-      $('#sf_wind_speed').text(resp.query.results.channel.wind.speed);
+// JavaScript script that displays the value of hello
+
+$(() => {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', (data, textStatus) => {
+    if (textStatus === 'success') {
+      $('div#hello').text(data.hello);
     }
   });
 });
