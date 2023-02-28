5b640826540880c0193b5fc7bb7f73baf99c4572
felixtoo48 <felixtoo48@gmail.com>
2023-02-28 14:50:17 +0300

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // A script that reads and prints the content of a file
 
-let filename = process.argv[2];
+const file = process.argv[2];
 const fs = require('fs');
-fs.readFile(filename, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
+
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data);
   }
