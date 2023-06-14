ee2fe82f1b83ade22923e99194659f7c74b821fa
felixtoo48 <felixtoo48@gmail.com>
2023-06-14 23:58:51 +0300

    documentation error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const argsCount = process.argv.lenth - 2;
-if (argsCount === 2) {
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
   console.log("No argument");
-} else if (argsCount === 3) {
+} else if (argsCount === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
